52d3b2f3ce211a42635eeb7859d92b7955261196
Mitchell Gertrude <mitchgertrude222@gmail.com>
2024-09-09 00:01:33 +0000

    Initial commit

---
+++ src/ErrorPage.tsx
@@ -0,0 +1,27 @@
+import Footer from "./component/Footer";
+import NavBar from "./component/NavBar";
+
+const ErrorPage = () => {
+  return (
+    <>
+      <NavBar />
+
+      <section>
+        <div className="w-full h-screen flex flex-col justify-center items-center">
+          <p className="text-white text-7xl font-medium">Uh oh!</p>
+          <p className="text-zinc-300 text-xl">This page doesn't exist.</p>
+          <a
+            className="px-5 py-3 text-white bg-indigo-800 hover:bg-indigo-700 rounded-lg mt-10"
+            href="/"
+          >
+            Go Back
+          </a>
+        </div>
+      </section>
+
+      <Footer />
+    </>
+  );
+};
+
+export default ErrorPage;
---
+++ src/FaqPage.tsx
@@ -0,0 +1,11 @@
+import NavBar from "./component/NavBar";
+
+const FaqPage = () => {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+};
+
+export default FaqPage;
